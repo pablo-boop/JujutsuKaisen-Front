1bd62638c8cc05dc74466c119f469fbbd016c122
Lucas Rios <lucas.rios@aluno.senai.br>
2023-11-23 15:22:19 -0300

    comentarios

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -7,21 +7,21 @@ import styles from './footer.module.css'
 
 const Footer = () => {
     return(
-        <div className={styles.footer}>
+        <div className={styles.footer}> {/*Footer body*/}
 
-         <p className={styles.texto}>©Copyright-2023 Todos os direitos reservados.</p>
+         <p className={styles.texto}>©Copyright-2023 Todos os direitos reservados.</p> {/*texto direitos reservados*/}
 
-        <div className={styles.sociais}>
+        <div className={styles.sociais}> {/*Links para redes sociais e icones*/}
         <Link href={'https://www.instagram.com/'} target='_blank' className={styles.link}>
-          <AiFillInstagram className={styles.icone} />
+          <AiFillInstagram className={styles.icone} /> {/*icone Instagram*/}
         </Link>
 
         <Link href={'https://www.twitter.com/'} target='_blank' className={styles.link}>
-          <AiFillTwitterCircle className={styles.icone} />
+          <AiFillTwitterCircle className={styles.icone} /> {/*icone Twitter*/}
         </Link>
 
         <Link href={'https://www.gmail.com/'} target='_blank' className={styles.link}>
-          <AiFillMail className={styles.icone} />
+          <AiFillMail className={styles.icone} />  {/*icone Gmail*/}
         </Link>
         </div>
 
--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -3,15 +3,18 @@ import styles from './header.module.css';
 
 const Header = () => {
     return(
-    <div className={styles.header}>
+    <div className={styles.header}>  {/*div Header body*/}
 
+         {/*imagem itadori*/}
         <img src='bandai-jujutsu-kaisen-yuji-itadori-001-1000x1000 (1).webp' width={100} height={100}></img>
 
+        {/*imagem da logo*/}
         <div className={styles.img}>
             <img src='sorcery-fight-5f2f76df8ce9a.png' width={210} height={110}></img>
         </div>
 
         <nav className={styles.nav}>
+         {/*lista de links para as outras páginas*/}
         <ul className={styles.ul}>
         <li><Link href={'/'}>Home</Link></li>
         <li><Link href={'/'}>Contato</Link></li>
