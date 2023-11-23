f288e076c6bafa1e4a6778d603a37e17c3dabfce
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-11-23 16:12:03 -0300

    mecanicas e regras

--- app/page.jsx
+++ app/page.jsx
@@ -52,6 +52,27 @@ export default function Home() {
                nível de força, começando pelo nível 4 até o 1 e também o nível especial;
             </p>
         </div>
+        {/*texto explicando as regras*/}
+        <div id={styles.regras}>
+          <h2>Mecânicas e regras:</h2>
+          <p>
+            No começo de todo o jogo é somado o ataque e a defesa de todos personagens, se o valor da soma de
+             todos os personagens for maior que uma média específica a mão é rerolada para balancear as duas
+              mãos;
+          </p>
+          <p>
+            Quando a batalha começar será selecionado um personagem de cada lado, a ganhador será definido pelo
+             maior MP ( Média por personagem );
+          </p>
+          <p>
+          Dedo do Sukuna: Se em uma partida começar e em sua mão tiver o personagem Itadori e a carta de efeito
+           do “Dedo do Sukuna”, o dedo sumirá e será sumonado na sua mão a carta do Sukuna;
+          </p>
+          <p>
+          Expansão de domínio: Uma carta de efeito que possui a funcionalidade de aumentar um NP do personagem
+           escolhido aumentando o ataque e a defesa do personagem;
+          </p>
+        </div>
       </div>
     </main>
   )
