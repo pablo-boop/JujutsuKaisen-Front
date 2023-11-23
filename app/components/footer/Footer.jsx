fa585e3f28ee14f09f404d7542edb1c006a911ea
Lucas Rios <lucas.rios@aluno.senai.br>
2023-11-23 10:34:44 -0300

    footer html

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,8 +1,19 @@
+import { AiFillInstagram,
+         AiFillTwitterCircle,
+         AiFillMail } from "react-icons/ai";
 import styles from './footer.module.css'
 
 const Footer = () => {
     return(
         <div className={styles.footer}>
+
+         <p>©2023 Todos os direitos reservados.</p>
+
+        <div className={styles.icones}>
+          <AiFillInstagram />
+          <AiFillTwitterCircle />
+          <AiFillMail />
+        </div>
 
         </div>
     )
