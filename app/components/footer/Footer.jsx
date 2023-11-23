f1b5f550e5a132839ecefa658d96fcf02b8773d9
Lucas Rios <lucas.rios@aluno.senai.br>
2023-11-23 10:21:42 -0300

    inicio do footer

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from './footer.module.css'
+
+const Footer = () => {
+    return(
+        <div className={styles.footer}>
+
+        </div>
+    )
+}
+export default Footer;
--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import styles from './header.module.css';
 
 const Header = () => {
+    return(
     <div className={styles.header}>
 
         <img src='bandai-jujutsu-kaisen-yuji-itadori-001-1000x1000 (1).webp' width={100} height={100}></img>
 
         <div className={styles.img}>
-            <img src='sorcery-fight-5f2f76df8ce9a.png' width={200} height={100}></img>
+            <img src='sorcery-fight-5f2f76df8ce9a.png' width={210} height={110}></img>
         </div>
 
         <nav className={styles.nav}>
@@ -18,6 +19,7 @@ const Header = () => {
         </ul>
       </nav>
     </div>
+    )
 }
 
 export default Header;
