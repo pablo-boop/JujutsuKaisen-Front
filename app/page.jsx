8a4e9aba0efdfb26569ac24843ec764c0c629043
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-11-28 16:48:09 -0300

    New component for home page

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ import { useScroll } from 'framer-motion';
 import Parallax from './components/Parallax/Parallax';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import ContentSection from './components/ContentSection/ContentSection';
 
 export default function Home() {
   //API
@@ -32,51 +33,13 @@ export default function Home() {
 
   return (
     <>
-    <Header />
+       <Header />
       <main className={styles.main}>
         <Parallax />
-        {/*explicação do anime*/}
-
-        {/*explicação do jogo*/}
         <div className={styles.content_container}>
-          <h1>Explicação do jogo</h1>
-          {/*texto explicando o TCG*/}
-          <div className={styles.content}>
-            <h3>Explicação do TCG:</h3>
-            <p>Um sistema de cartas com dois tipos principais sendo as cartas de personagem e as cartas de efeito.
-              No começo de cada jogo é sorteado 5 cartas de personagens e 2 cartas de efeito, o objetivo é
-              conseguir derrotar os 5 personagens do adversário para ganhar a partida.
-            </p>
-          </div>
-          {/*texto explicando frases tecnicas*/}
-          <div className={styles.content}>
-            <h3>Frases Técnicas:</h3>
-            <p>-MP ( Média por personagem ), é a soma do ataque e a defesa do personagem;</p>
-            <p> -NP ( Nível de personagem ): Todo personagem possui um ranking que define sua raridade e o seu
-              nível de força, começando pelo nível 4 até o 1 e também o nível especial;
-            </p>
-          </div>
-          {/*texto explicando as regras*/}
-          <div className={styles.content}>
-            <h3>Mecânicas e regras:</h3>
-            <p>
-              No começo de todo o jogo é somado o ataque e a defesa de todos personagens, se o valor da soma de
-              todos os personagens for maior que uma média específica a mão é rerolada para balancear as duas
-              mãos;
-            </p>
-            <p>
-              Quando a batalha começar será selecionado um personagem de cada lado, a ganhador será definido pelo
-              maior MP ( Média por personagem );
-            </p>
-            <p>
-              Dedo do Sukuna: Se em uma partida começar e em sua mão tiver o personagem Itadori e a carta de efeito
-              do “Dedo do Sukuna”, o dedo sumirá e será sumonado na sua mão a carta do Sukuna;
-            </p>
-            <p>
-              Expansão de domínio: Uma carta de efeito que possui a funcionalidade de aumentar um NP do personagem
-              escolhido aumentando o ataque e a defesa do personagem;
-            </p>
-          </div>
+          <ContentSection title="Explicação do TCG" text="Um sistema de cartas com dois tipos principais sendo as cartas de personagem e as cartas de efeito. No começo de cada jogo é sorteado 5 cartas de personagens e 2 cartas de efeito, o objetivo é conseguir derrotar os 5 personagens do adversário para ganhar a partida." />
+          <ContentSection title="Frases Técnicas" text="-MP ( Média por personagem ), é a soma do ataque e a defesa do personagem; -NP ( Nível de personagem ): Todo personagem possui um ranking que define sua raridade e o seu nível de força, começando pelo nível 4 até o 1 e também o nível especial;" />
+          <ContentSection title="Mecânicas e regras" text="No começo de todo o jogo é somado o ataque e a defesa de todos personagens, se o valor da soma de todos os personagens for maior que uma média específica a mão é rerolada para balancear as duas mãos; Quando a batalha começar será selecionado um personagem de cada lado, a ganhador será definido pelo maior MP ( Média por personagem ); Dedo do Sukuna: Se em uma partida começar e em sua mão tiver o personagem Itadori e a carta de efeito do “Dedo do Sukuna”, o dedo sumirá e será sumonado na sua mão a carta do Sukuna; Expansão de domínio: Uma carta de efeito que possui a funcionalidade de aumentar um NP do personagem escolhido aumentando o ataque e a defesa do personagem;" />
         </div>
       </main>
       <Footer />
