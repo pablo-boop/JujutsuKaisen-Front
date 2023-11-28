b4cc868631798db5f89254785461681ec3443b43
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-11-28 09:10:44 -0300

    done jujst need the pop up

--- app/Contato/page.jsx
+++ app/Contato/page.jsx
@@ -2,10 +2,22 @@ import styles from "./contato.module.css"
 const contato = ()=> {
     return(
         <div className={styles.geral}>
+            <img src="https://i.pinimg.com/originals/7f/a2/43/7fa243b1a78cc227e3fed2d878cfecc8.gif" alt="mimi" className={styles.gif} />
             <div className={styles.card}>
-                <input placeholder="Nome" type="text" />
-                <input type="text" placeholder="E-mail" />
-                <input type="text" placeholder="Comentario" />
+                <h1 className={styles.h1}>CONTATO</h1>
+                <hr className={styles.hrr} />
+                <div className={styles.gi}>
+                    <section>
+                
+                </section>
+                <section className={styles.sec}>
+                <input placeholder="Nome" type="text" className={styles.inputs} />
+                <input type="text" placeholder="E-mail" className={styles.inputs} />
+                <input type="text" placeholder="Comentario" className={styles.coment} />
+                <button className={styles.button}>Enviar</button>
+                </section>
+                </div>
+                
             </div>
         </div>
     )
