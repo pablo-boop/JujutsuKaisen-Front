4f517fbd2abac0bbdee16da1709876e65e9de792
Pablo Couto <pablocouto@aluno.senai.br>
2023-11-30 14:49:52 -0300

    feat: concertado erros

--- app/api/cards/route.js
+++ app/api/cards/route.js
@@ -1,36 +1,30 @@
 import axios from "axios";
 
-//obtem a URL
+import { NextResponse } from "next/server";
+
 const url = process.env.BASE_URL;
 
-//função que trata as requisições GET
-export async function GET(req, res) {
+export async function GET() {
     try {
         const response = await axios.get(url);
 
-        //retorna uma resposta para a próxima camada de aplicação com os dados recebidos
-        return res.status(200).json(response.data);
+        return NextResponse.json(response.data);
     } catch (error) {
         console.log("[ORDER_GET]", error);
-        //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
-        return res.status(500).send("Erro interno do servidor!");
+        return new NextResponse("Erro interno do servidor!", { status: 500 });
     }
 }
 
-//função que trata as requisições POST
-export async function POST(req, res) {
-    //extrai os parametros das requisições 
-    const params = await req.json();
+export async function POST(request) {
+    const params = await request.json();
 
     try {
         const response = await axios.post(url, params);
 
-         //retorna uma resposta com os dados recebidos.
-        return res.status(200).json(response.data);
+        return NextResponse.json(response.data);
     } catch (error) {
-         //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
         console.log("[ORDER_POST]", error);
-        return res.status(500).send("Erro interno do servidor!");
+        return new NextResponse("Erro interno do servidor!", { status: 500 });
     }
 }
 
@@ -42,10 +36,10 @@ export async function PUT(req, res) {
     try {
         const response = await axios.put(url, params);
 
-         //retorna uma resposta com os dados recebidos.
+        //retorna uma resposta com os dados recebidos.
         return res.status(200).json(response.data);
     } catch (error) {
-         //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
+        //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
         console.log("[ORDER_PUT]", error);
         return res.status(500).send("Erro interno do servidor!");
     }
@@ -59,10 +53,10 @@ export async function DELETE(req, res) {
     try {
         const response = await axios.delete(`${url}/${params.id}`);
 
-         //retorna uma resposta com os dados recebidos.
+        //retorna uma resposta com os dados recebidos.
         return res.status(200).json(response.data);
     } catch (error) {
-         //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
+        //em caso de erro nas requisições, retorna uma mensagem de erro interno no servidor
         console.log("[ORDER_DELETE]", error);
         return res.status(500).send("Erro interno do servidor!");
     }
