fd758eb5ca87839e37326db30056fc4df1cf87ac
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-11-30 11:17:14 -0300

    não sei oq escrever

--- app/components/ContentSection/ContentSection.jsx
+++ app/components/ContentSection/ContentSection.jsx
@@ -6,12 +6,11 @@ import styles from './ContentSection.module.css';
 // Este componente recebe duas props: title, text e gif
 // title: O título da seção de conteúdo
 // text: O texto da seção de conteúdo
-export default function ContentSection({ title, text, gif }) {
+export default function ContentSection({ title, text}) {
     // O componente retorna um div com uma classe de estilo 'content'
     // Dentro deste div, temos um h3 que exibe o título e um p que exibe o texto
     return (
       <div className={styles.content}>
-        <img src={gif} alt="gif legal" />
         <h3>{title}</h3>
         <p>{text}</p>
       </div>
