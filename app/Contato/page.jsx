ffdbd1e7b5b0357d8649468bfc6e1e49b38c5f04
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-11-30 15:41:55 -0300

    fixed all problems

--- app/Contato/page.jsx
+++ app/Contato/page.jsx
@@ -21,6 +21,10 @@ const contato = () => {
         if (verifyInputs()) {
             // Se todos os campos estiverem preenchidos, faça algo aqui
             alert("Parabéns, você nos contatou!");
+                // Limpar os inputs
+                setName("");
+                setEmail("");
+                setComment("");
         }
     }
 
@@ -49,7 +53,7 @@ const contato = () => {
                         <input placeholder="Nome" value={name} onChange={(e) => setName(e.target.value)} type="text" className={styles.inputs} />
                         <input type="text" placeholder="E-mail" className={styles.inputs} value={email} onChange={(e) => setEmail(e.target.value)} />
                         <input type="text" placeholder="Comentario" className={styles.coment} value={comment} onChange={(e) => setComment(e.target.value)} />
-                        <ModalPopUp buttonTitle={"CONTATAR"} modalTitle={"Você nos contatou, parabéns!"} modalContent={"Não vou ver."} modalImg={"https://i.pinimg.com/originals/68/f1/42/68f142cc473e62240c593f7290d263ab.gif"} style={styles.modalBtn} />
+                        <button onClick={handleSubmit} className={styles.modalBtn}>CONTATAR</button>
                     </section>
                 </div>
             </div>
