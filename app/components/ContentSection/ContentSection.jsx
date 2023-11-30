3e6a35a5324a3f413da1cbdf195046c88c22ebdf
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-11-30 10:22:08 -0300

    colocando imagem no componete

--- app/components/ContentSection/ContentSection.jsx
+++ app/components/ContentSection/ContentSection.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import styles from './ContentSection.module.css';
 
 // Componente ContentSection
-// Este componente recebe duas props: title e text
+// Este componente recebe duas props: title, text e gif
 // title: O título da seção de conteúdo
 // text: O texto da seção de conteúdo
-export default function ContentSection({ title, text }) {
+export default function ContentSection({ title, text, gif }) {
     // O componente retorna um div com uma classe de estilo 'content'
     // Dentro deste div, temos um h3 que exibe o título e um p que exibe o texto
     return (
       <div className={styles.content}>
+        <img src={gif} alt="gif legal" />
         <h3>{title}</h3>
         <p>{text}</p>
       </div>
