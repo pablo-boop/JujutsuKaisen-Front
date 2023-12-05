d69a5b125852701071bc465e832f67b4c7bcbbd2
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-12-05 09:17:13 -0300

    done contato

--- app/Contato/page.jsx
+++ app/Contato/page.jsx
@@ -1,6 +1,5 @@
 "use client"
 import styles from "./contato.module.css"
-import ModalPopUp from "../components/Modal/Modal"
 import { motion } from 'framer-motion';
 import { useState } from "react";
 
@@ -9,15 +8,26 @@ const contato = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [comment, setComment] = useState("");
-    const [modalContent, setModalContent] = useState("Não vou ver.");
 
     const verifyInputs = () => {
         if (!name || !email || !comment) {
-            setModalContent("Por favor, preencha todos os campos.");
-        } else {
-            setModalContent("Você nos contatou, parabéns!");
+            alert("Por favor, preencha todos os campos.");
+            return false;
+        }
+        return true;
+    }
+
+    const handleSubmit = () => {
+        if (verifyInputs()) {
+            // Se todos os campos estiverem preenchidos, faça algo aqui
+            alert("Parabéns, você nos contatou!");
+                // Limpar os inputs
+                setName("");
+                setEmail("");
+                setComment("");
         }
     }
+
     return (
         <div className={styles.geral}>
             <motion.img
@@ -43,7 +53,11 @@ const contato = () => {
                         <input placeholder="Nome" value={name} onChange={(e) => setName(e.target.value)} type="text" className={styles.inputs} />
                         <input type="text" placeholder="E-mail" className={styles.inputs} value={email} onChange={(e) => setEmail(e.target.value)} />
                         <input type="text" placeholder="Comentario" className={styles.coment} value={comment} onChange={(e) => setComment(e.target.value)} />
-                        <ModalPopUp buttonTitle={"CONTATAR"} modalTitle={"Você nos contatou, parabéns!"} modalContent={modalContent} modalImg={"https://i.pinimg.com/originals/68/f1/42/68f142cc473e62240c593f7290d263ab.gif"} style={styles.modalBtn} onClick={verifyInputs} />
+                        <motion.button
+                         onClick={handleSubmit}
+                         whileHover={{ scale: 1.1 }}
+                         whileTap={{ scale: 0.9 }}
+                         className={styles.modalBtn}>Contatar</motion.button>
                     </section>
                 </div>
             </div>
