2947ce5c29a592a3964f6b42351c0b86df9c953c
Lucas Rios <lucas.rios@aluno.senai.br>
2023-12-05 17:07:14 -0300

    alterações

--- app/S/page.jsx
+++ app/S/page.jsx
@@ -2,35 +2,75 @@
 import React, { useState } from 'react';
 import Popup from '../components/PopUp/PopUp';
 
-const App = () => {
+ /* const App = () => {
   const [showPopup, setShowPopup] = useState(true);
   const [popUpMessage, setPopUpMessage] = useState('');
-  const [popUpType, setPopUpType] = useState ('');
+  const [popUpBackground, setPopUpBackground] = useState ('');
 
-  function handleShowPopUp (text, type, time){
-    setPopUpMessage(text);
-    setPopUpType(type);
+  function handleShowPopUp (background, time){
+    setPopUpBackground(background);
+    setPopUpMessage(message);
     setShowPopup (true);
     setTimeout (() => {
       setShowPopup (false)
     }, time)
-  }
-
+  }  
+  
   return (
-    <div className="App">
-      <button onClick={() => handleShowPopUp ('Voce Ganhou!', 'victory')}>
-        Mensagem de Vitória
-      </button>
-      <button onClick={() => handleShowPopUp ('Voce Perdeu!', 'lose')}>
-        Mensagem de Derrota
-      </button>
-      <button onClick={() => handleShowPopUp ('Empate!', 'gameTied')}>
-        Mensagem de Empate
-      </button>
+      <div>
+        <button onClick={() => handleShowPopUp('url(https://media.tenor.com/KBnATdctL1MAAAAC/jujutsu-kaisen-jujutsu-kaisen-dance.gif)', 'Voce ganhou!')}>Vitória</button>
+        <button onClick={() => handleShowPopUp('url(https://media.tenor.com/iEvzToGb7W0AAAAC/nanami-shigemo.gif)', 'Voce Perdeu!')}>Derrota</button>
+        <button onClick={() => handleShowPopUp('url(https://media.tenor.com/m1arMEn09NcAAAAd/todo-aoi-todo.gif)', 'Empate!')}>Empate</button>
+     
+      { <Popup showPopup={showPopup} message={popUpMessage} /> }
 
-       { <Popup showPopup={showPopup} text={popUpMessage} type={popUpType} /> }
     </div>
   );
 };
 
-export default App;
+export default App; */
+
+const PopupWithBackground = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupBackground, setPopupBackground] = useState('');
+  const [popupMessage, setPopupMessage] = useState('');
+
+  const handleShowPopUp = (background, message) => {
+    setShowPopup(true);
+    setPopupBackground(background);
+    setPopupMessage(message);
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleShowPopUp('url(https://media.tenor.com/KBnATdctL1MAAAAC/jujutsu-kaisen-jujutsu-kaisen-dance.gif)', 'Voce ganhou!')}>Vitória</button>
+      <button onClick={() => handleShowPopUp('url(https://media.tenor.com/iEvzToGb7W0AAAAC/nanami-shigemo.gif)', 'Voce Perdeu!')}>Derrota</button>
+      <button onClick={() => handleShowPopUp('url(https://media.tenor.com/m1arMEn09NcAAAAd/todo-aoi-todo.gif)', 'Empate!')}>Empate</button>
+
+      {showPopup && (
+        <div
+          style={{
+            backgroundImage: popupBackground,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            padding: '20px',
+            color: 'white',
+            borderRadius: '15px',
+            zIndex: 9999,
+            width: '600px', // largura do pop-up
+            height: '300px', // altura do pop-up
+          }}
+        >
+          {popupMessage}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PopupWithBackground;
+
--- app/components/PopUp/PopUp.jsx
+++ app/components/PopUp/PopUp.jsx
@@ -2,44 +2,30 @@
 import React, { useState, useEffect } from 'react';
 import style from './popUp.module.css'
 
-const Popup = ({ showPopup, text, type }) => {
-  const [isVisible, setIsVisible] = useState(showPopup);
-
- /* useEffect(() => {
-    let timeoutId;
-
-    if (isVisible) {
-      timeoutId = setTimeout(() => {
-        setIsVisible(false);
-      }, 10000); // Definindo a duração para 10 segundos (10000 milissegundos)
-    }
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [isVisible]); */
+const Popup = ({ background, message }) => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupBackground, setPopupBackground] = useState('');
+  const [popupMessage, setPopupMessage] = useState('');
 
   const handleClose = () => {
-    setIsVisible(false);
+    setShowPopup(false);
   };
 
-  let gameResult = '';
-
-  if(type === 'victory'){
-    gameResult = style.victory //se o resultado do jogo for uma Vitória
-  } else if (type === 'lose'){
-    gameResult = style.lose  //se o resultado do jogo for uma Derrota
-  } else{
-    gameResult = style.gameTied //se o resultado do jogo for um Empate
+  const handleShowPopUp = (background, message) => {
+    setShowPopup(true);
+    setPopupBackground(background);
+    setPopupMessage(message);
   }
 
   return (
     <div>
-      {isVisible && (
-        <div className={style.popup}>
+
+      {showPopup && (
+        <div className={style.popup}  style={{
+          backgroundImage: `url(${background})`}}>
           <div className={style.popupContent}>
-            <div className={style.popupText}>
-              <h1>{text}</h1>
+            <div className={style.popupMessage}>
+              <h1>{message}</h1>
             </div>
             <button onClick={handleClose} className={style.botao}>
               Fechar
