0515766dfa11331049bfedfa33d45cf6a05baeac
Lucas Rios <lucas.rios@aluno.senai.br>
2023-12-12 08:25:31 -0300

    comentando códigos

--- app/S/page.jsx
+++ app/S/page.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [popupBackground, setPopupBackground] = useState('');
   const [popupMessage, setPopupMessage] = useState('');
 
+  //alternar
   const togglePopup = (background, message) => {
     setShowPopup(true);
     setPopupBackground(background);
@@ -15,6 +16,7 @@ const App = () => {
 
   };
 
+  //fechar PopUp
   const closePopup = () => {
     setShowPopup(false);
 
@@ -22,15 +24,18 @@ const App = () => {
 
   return (
     <div>
+      {/*vitória*/}
       <button onClick={() => togglePopup('url(https://media1.tenor.com/m/KBnATdctL1MAAAAC/jujutsu-kaisen-jujutsu-kaisen-dance.gif)', 'Voce ganhou!')}>Vitória</button>
+      {/*derrota*/}
       <button onClick={() => togglePopup('url(https://media1.tenor.com/m/iEvzToGb7W0AAAAC/nanami-shigemo.gif)', 'Voce perdeu!')}>Derrota</button>
+      {/*empate*/}
       <button onClick={() => togglePopup('url(https://media1.tenor.com/m/m1arMEn09NcAAAAd/todo-aoi-todo.gif)', 'Empate!')}>Empate</button>
 
       <Popup
         showPopup={showPopup}
-        popupBackground={popupBackground}
-        popupMessage={popupMessage}
-        onClose={closePopup}
+        popupBackground={popupBackground} //Fundo do pop up
+        popupMessage={popupMessage} // mensagem do pop up
+        onClose={closePopup} //fechar pop up
       />
     </div>
   );
